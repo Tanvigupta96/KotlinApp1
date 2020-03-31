fc80e7cb5a6a1b86c65c56f5796f62dc190b634f
Tanvigupta96 <guptatannu.34gmail.com>
2020-03-31 17:19:07 +0530

    second Commit

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -11,27 +11,23 @@ import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var userNameEditText : EditText
-    private lateinit var passwordEditText : EditText
-    private lateinit var sign_in_btn : Button
-    private lateinit var sign_up_txt : TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initializeFields()
 
-        sign_in_btn.setOnClickListener {
-            val username : String = userNameEditText.text.toString()
-            val pass : String = passwordEditText.text.toString()
+        sign_in_button.setOnClickListener {
+            val username : String = login_username.text.toString()
+            val pass : String = login_password.text.toString()
             if(TextUtils.isEmpty(username)){
                 Toast.makeText(this,"Enter Username FirstName",LENGTH_SHORT).show()
             }
-            if(TextUtils.isEmpty(pass)){
+            else if(TextUtils.isEmpty(pass)){
                 Toast.makeText(this,"Enter Password first", LENGTH_SHORT).show()
             }
             else{
@@ -43,15 +39,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initializeFields() {
-        userNameEditText = findViewById(R.id.login_username)
-        passwordEditText = findViewById(R.id.login_password)
-        sign_in_btn = findViewById(R.id.sign_in_button)
-        sign_up_txt = findViewById(R.id.sign_up_txt)
 
-
-
-    }
 }
 
 
